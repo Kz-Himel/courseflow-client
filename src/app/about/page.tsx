790097faf645ceb2664ebe4f1697277bcc777a30
Kz-Himel <kzhimel129@gmail.com>
2026-07-13 12:55:12 +0600

    Added: About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 px-4 py-8 md:px-8">
+      <div className="max-w-6xl mx-auto">
+        About Page
+      </div>
+    </div>
+  );
+}
