30004ed12161c75270d53ce477535b1c4cb98828
Kz-Himel <kzhimel129@gmail.com>
2026-07-15 14:26:23 +0600

    Added: Some changes in lib

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,36 @@
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+// import { jwt } from "better-auth/plugins";
+
+// const client = new MongoClient(process.env.MONGODB_URI!);
+// const db = client.db(process.env.DATABASE_NAME!);
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+//     // Optional: if you don't provide a client, database transactions won't be enabled.
+//     client
+//   }),
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+//   socialProviders: {
+//     google: { 
+//       clientId: process.env.GOOGLE_CLIENT_ID as string, 
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+//     }, 
+//   },
+//   session: {
+//     cookieCache: {
+//       enabled: true,
+//       strategy: "jwt",
+//       maxAge: 7 * 24 * 60 * 60,
+//     },
+//   },
+
+//   plugins: [jwt()],
+// });
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -7,8 +40,12 @@ const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db(process.env.DATABASE_NAME!);
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.BETTER_AUTH_URL!,
+  ],
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
   emailAndPassword: {
